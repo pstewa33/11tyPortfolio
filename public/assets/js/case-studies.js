5c56a4db748a68864c847eb3ce7107cadb89ef6c
Ashlen33 <peyton.stewart33@gmail.com>
2026-08-06 16:17:34 -0400

    foundation is there for images in dmx case study

--- public/assets/js/case-studies.js
+++ public/assets/js/case-studies.js
@@ -1,16 +1,36 @@
-document.querySelectorAll(".solution-images").forEach(container => {
+const images = [...document.querySelectorAll(".solution-image")];
+const counter = document.getElementById("current-image");
+const caption = document.getElementById("image-caption");
 
-    const images = container.querySelectorAll(".solution-image");
+function updateImageInfo() {
+  const activeImage = document.querySelector(".solution-image.active");
+  const current = images.indexOf(activeImage);
 
-    images.forEach(image => {
+  counter.textContent = current + 1;
+  caption.textContent = activeImage.alt;
+}
 
-        image.addEventListener("click", () => {
+function rotateImages() {
+  images.forEach(img => {
+    if (img.classList.contains("active")) {
+      img.classList.replace("active", "back");
+    } else if (img.classList.contains("middle")) {
+      img.classList.replace("middle", "active");
+    } else {
+      img.classList.replace("back", "middle");
+    }
+  });
 
-            images.forEach(img => img.classList.remove("active"));
-            image.classList.add("active");
+  updateImageInfo();
+}
 
-        });
-
-    });
-
+images.forEach(img => {
+  img.addEventListener("click", rotateImages);
 });
+
+document
+  .querySelector(".image-next")
+  .addEventListener("click", rotateImages);
+
+// Set the initial caption and counter
+updateImageInfo();
